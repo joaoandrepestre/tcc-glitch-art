a4238d440b3e0c9a1a1d0a1d069ee822043df536
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2022-05-29 19:15:31 -0300

    remove old code

--- gui/index.js
+++
@@ -1,6 +0,0 @@
-import { Gui } from './gui.js';
-
-window.onload = async () => {
-  const gui = new Gui();
-  gui.update();
-};
--- gui/utils.js
+++
@@ -1,6 +0,0 @@
-const tryGetValue = (obj, key, dflt) => {
-  if (!(key in obj)) obj[key] = dflt;
-  return obj[key];
-};
-
-export { tryGetValue };
